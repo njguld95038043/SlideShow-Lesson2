b5d6fd58905980a2620970037fbbb83c4b31de05
t t <tt@tinoMacBook-Air.local>
2022-01-24 19:24:36 +0900

    [Finish]

---
+++ js/script.js
@@ -0,0 +1,12 @@
+$(".theTarget").skippr({
+  transition: 'fade',
+  spped : 1000,
+  easing : 'easeOutQuart',
+  navType : 'bubble',
+  childrenElementType : 'div',
+  arrows : true,
+  autoPlay : true,
+  autoPlayDuration : 5000,
+  keyboardOnAlways : true,
+  hidePrevious : false
+});
